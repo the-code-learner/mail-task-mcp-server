cbca3dc397486090b85bda6ca074a8238612f40c
the-code-learner <142033899+the-code-learner@users.noreply.github.com>
2026-08-23 00:32:10 +0200

    v9.6.0: install Knowledge scope runtime

--- src/postmaster/runtime.py
+++ src/postmaster/runtime.py
@@ -9,6 +9,7 @@ from .runtime_v946 import install_runtime_v946
 from .runtime_v950 import install_runtime_v950
 from .runtime_v953 import install_runtime_v953
 from .runtime_v960 import install_runtime_v960
+from .runtime_v960_knowledge import install_runtime_v960_knowledge
 from .webgui_tasks import task_fragment as _task_fragment_v945
 from .webgui_v945 import install_webgui_v945
 from .webgui_v951 import install_webgui_v951
@@ -43,6 +44,7 @@ build_status = install_runtime_v953(_base, _core, build_status)
 dashboard_home, build_status, mail_client = install_runtime_v960(
     app, _base, _core, dashboard_home, build_status
 )
+install_runtime_v960_knowledge(_base, _core)
 dashboard_home = install_webgui_v951(app, _base, _core, dashboard_home)
 dashboard_home = install_webgui_v952(app, _base, dashboard_home)
 dashboard_home = install_webgui_v953(app, _base, _core, dashboard_home)
